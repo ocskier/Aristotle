acb6f9c2267837e8d8dda5cdd3d81602ddf6ee00
mistmr7 <michael.mistarz@gmail.com>
2018-09-24 13:37:14 -0400

    fixed api routes, need to figure out what goes in the post where req.body currently is

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,7 +27,8 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    var queryVar = _.capitalize(req.params.subject);
+    db[queryVar].destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
