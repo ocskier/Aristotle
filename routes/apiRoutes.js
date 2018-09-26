889ef66761d038e5c725d6af94fa2dac5cfa7810
mistmr7 <michael.mistarz@gmail.com>
2018-09-26 19:06:14 -0400

    commit model changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,22 +27,6 @@ module.exports = function(app) {
     });
   });
 
-  // create a new user at signup
-  //   app.post("/api/signup", function(req, res) {
-  //     console.log(req.body);
-  //     console.log(req.body.name);
-  //     db.User.create({
-  //       name: req.body.name,
-  //       email: req.body.email,
-  //       password: req.body.password,
-  //       subject: req.body.subject,
-  //       ageGroup: req.body.ageGroup
-  //       // region: req.body.region
-  //     }).then(function(response) {
-  //       res.json(response);
-  //     });
-  //   });
-
   // Delete a user by id
   app.delete("/api/users/:id", function(req, res) {
     db.User.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
