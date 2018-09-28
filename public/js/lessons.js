560d1eee715dc2d411d3c0c3b56a72544ade8f61
Pack Fan 1031 <ocskier@gmail.com>
2018-09-28 16:01:17 -0400

    styling changes

--- public/js/lessons.js
+++ public/js/lessons.js
@@ -1,27 +1,35 @@
 function runLessonsQuery() {
   // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
-  $.ajax({ url: "/api/lessons", method: "GET" }).then(function(data) {
-    // Here we then log the tableData to console, where it will show up as an object.
-    console.log(data);
-    console.log("------------------------------------");
+  $.ajax({ url: "/api/user_data", method: "GET" }).then(function(data) {
+    var subject = "math";
+    var ageGroup = "Elementary";
 
-    // Loop through and display each of the customers
-    for (var i = 0; i < data.length; i++) {
-      // Get a reference to the tableList element and populate it with tables
-      var lessonsList = $("#lessons-row");
+    $.ajax({ url: "/api/" + ageGroup + "/" + subject, method: "GET" }).then(
+      function(data) {
+        // Here we then log the tableData to console, where it will show up as an object.
+        console.log(data);
+        console.log("------------------------------------");
 
-      var newCardA = $(
-        '<a class="carousel-item" href="" target="blank" style="width:310px;"></a>'
-      );
-      newCardA
-        .append(
-          $(
-            '<img src="https://images.unsplash.com/photo-1528459199957-0ff28496a7f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a17975ae0832c15daee9763fe6ad9892&auto=format&fit=crop&w=682&q=80" alt="Portfolio Pic">'
-          )
-        )
-        .append($('<p class="ctr-txt white-text">Lesson Plan</p>')); //data[i].text
-      lessonsList.append(newCardA);
-    }
+        // Loop through and display each of the customers
+        for (var i = 0; i < data.length; i++) {
+          // Get a reference to the tableList element and populate it with tables
+          console.log(data[i]);
+          var lessonsList = $("#lessons-row");
+
+          var newCardA = $(
+            '<a class="carousel-item" href="" target="blank" style="width:310px;"></a>'
+          );
+          newCardA
+            .append(
+              $(
+                '<img src="https://images.unsplash.com/photo-1528459199957-0ff28496a7f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a17975ae0832c15daee9763fe6ad9892&auto=format&fit=crop&w=682&q=80" alt="Portfolio Pic">'
+              )
+            )
+            .append($('<p class="ctr-txt white-text">Lesson Plan</p>')); //data[i].text
+          lessonsList.append(newCardA);
+        }
+      }
+    );
   });
 }
 
