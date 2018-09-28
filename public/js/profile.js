184ced5151bf512ad69e64b4a25d2d5b5be55a3c
KamiKozy <kamikozytpp@gmail.com>
2018-09-27 21:03:35 -0400

    profilejs update

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,20 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   var textTitle = $("input#text-title");
   var textBody = $("input#text-body");
+  var textSubject = $("#subject-input-lesson");
+  var textAge = $("#age-input-lesson")
 
-  $("#submit-text").on("click", function(event) {
+  $("#submit-text").on("click", function (event) {
     event.preventDefault();
-    var userTextInput= {
+    var userTextInput = {
       title: textTitle.val().trim(),
       body: textBody.val().trim(),
+      subject: textSubject.val(),
+      ageGroup: textAge.val()
     };
 
     console.log(userTextInput);
 
     submitLesson(
       userTextInput.title,
-      userTextInput.body
+      userTextInput.body,
+      userTextInput.subject,
+      userTextInput.ageGroup
     );
 
     textTitle.val("");
@@ -22,10 +28,12 @@ $(document).ready(function() {
   });
 
   function submitLesson(title, body) {
-    $.post("api/*****", {
+    $.post("api/lesson", {
       title: title,
-      body: body
-    }).then(function(data) {
+      body: body,
+      subject: subject,
+      ageGroup: ageGroup
+    }).then(function (data) {
       console.log(data);
     });
   }
